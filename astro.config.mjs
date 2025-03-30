e366ac09ca936c5bc560a6c62b45977fe224a26c
Raghav Thakur <raghav@Raghavs-MacBook-Air.local>
2025-03-29 17:56:32 -0700

    Revert "Design overhaul"
    This reverts commit 60133e8ab6289b9a662b60867c5bdd726db38dd5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,17 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    trailingSlash: 'always',
-    site: 'https://raghavthakur.dev',
-
-    // Single page, no prefetch needed
-    prefetch: false,
-
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            }
-        })
-    ]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
