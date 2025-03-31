85a1a76c5a8ee1e1a7db19e44f1db27451fa01ca
Raghav Thakur <raghavthakur.dev@gmail.com>
2025-03-31 14:05:38 -0700

    Rewrite design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,41 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://raghavthakur.dev",
+  // single page, no prefetch needed
+  prefetch: false,
+  integrations: [mdx(), sitemap(), tailwind(), compress({
+    CSS: true,
+    SVG: false,
+    Image: false,
+    HTML: {
+        "html-minifier-terser": {
+            collapseWhitespace: true,
+            // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: true,
+            removeEmptyAttributes: true,
+            // removeEmptyElements: true, // It removes sometimes SVGs
+            removeRedundantAttributes: true
+        },
+    },
+    JavaScript: {
+        'terser': {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            }
+        }
+    }
+})],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
